f611575d62e1cfbc1c9fbdb9518009b9f64c9e87
Giorgi Ghughunishvili <gghughunishvili@users.noreply.github.com>
2021-03-08 07:07:32 -0500

    #37 added solution for problem 1332;

---
+++ js/1332_e.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
+    if (s === '') {
+        return 0;
+    }
+    let l = 0;
+    let r = s.length-1;
+    while (l<r) {
+        if (s[l] !== s[r]) {
+            return 2;
+        }
+        l++;
+        r--;
+    }
+    return 1;
+};
