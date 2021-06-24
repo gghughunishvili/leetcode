fda40acfd84197dbeb6ad391d9eb52ab3e4e71cf
Giorgi Ghughunishvili <gghughunishvili@users.noreply.github.com>
2021-06-24 19:49:13 -0400

    #42 added solution for 485;

---
+++ js/485_e.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let current = 0;
+    let max = 0;
+    for (let num of nums) {
+        if (num === 1) {
+            current++;
+        } else {
+            max = Math.max(max, current);
+            current = 0;
+        }
+    }
+    
+    max = Math.max(max, current);
+    
+    return max;
+};
