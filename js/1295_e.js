44ca59e5ca77543f3cd5070894c54465512afcbe
Giorgi Ghughunishvili <gghughunishvili@users.noreply.github.com>
2021-06-24 22:39:39 -0400

    #42 added second solution for problem 1295;

--- js/1295_e.js
+++ js/1295_e.js
@@ -11,3 +11,32 @@ var findNumbers = function(nums) {
     }
     return answer;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers2 = function(nums) {
+    let answer = 0;
+    
+    for (const n of nums) {
+        
+        if (getLengthOfDigits(n) % 2 === 0) {
+            answer++;
+        }
+        
+    }
+    
+    return answer;
+};
+
+var getLengthOfDigits = function(n) {
+    let count = 0;
+    
+    while (n != 0) {
+        n = Math.floor(n/10);
+        count++;
+    }
+    
+    return count;
+}
