a926d438e1e0a0d1bf8a8714a5bae93fd1c291c4
Giorgi Ghughunishvili <gghughunishvili@users.noreply.github.com>
2021-06-25 22:51:46 -0400

    #44 fixed commit for wrong issue;

--- js/1089_e.js
+++ js/1089_e.js
@@ -1,6 +1,6 @@
 /**
  * @param {number[]} arr
- * @return {void} Do not return anything, modify arr in-place instead.
+ * @return {void} Do not return anything, modify arr in-place instead...
  */
 var duplicateZeros = function(arr) {
     for (let i=0; i<arr.length-1; i++) {
