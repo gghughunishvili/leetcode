7211202881a2a0b35162aa8745dc1e3ee1746019
Giorgi Ghughunishvili <gghughunishvili@users.noreply.github.com>
2021-06-25 22:50:56 -0400

    #43 added solution for problem 1089;

---
+++ js/1089_e.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    for (let i=0; i<arr.length-1; i++) {
+        if (arr[i] === 0) {
+            for (let j = arr.length-1; j>i+1; j--) {
+                arr[j] = arr[j-1];
+            }
+            
+            arr[i+1] = 0;
+            i++;
+        }
+    }    
+};
