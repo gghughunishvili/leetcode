b442311bd56c6741515932e17c3e3056a77bfd14
Giorgi Ghughunishvili <gghughunishvili@users.noreply.github.com>
2021-09-29 22:07:34 -0400

    #47 added JS solution for problem 53;

--- js/53_e.js
+++ js/53_e.js
@@ -4,13 +4,13 @@
  */
 var maxSubArray = function(nums) {
 	let maxSum = nums[0];
-    let currentSum = 0;
+	let currentSum = 0;
 	for (let num of nums) {
 		currentSum += num;
 		maxSum = Math.max(maxSum, currentSum);
 		if (currentSum < 0) {
 			currentSum = 0;
 		}
-}
+	}
 	return maxSum;
 };
